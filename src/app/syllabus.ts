39610790ab3a50e94fca36f15599746de04c2756
Gabriel-Farrugia2000 <gabriel.farrugia.b31278@mcast.edu.mt>
2022-05-09 18:25:24 +0200

    Added data in the interface

--- src/app/syllabus.ts
+++ src/app/syllabus.ts
@@ -14,7 +14,7 @@ export interface Lesson
     question: Question;
 }
 
-export const EXAMPLE: Syllabus[] = [{
+export const lessons: Syllabus[] = [{
     name: 'Functions',
     slug: 'functions',
     lessons: [
@@ -53,7 +53,48 @@ export const EXAMPLE: Syllabus[] = [{
         }
     ]
 },
+{
+    name: 'Loops',
+    slug: 'loops',
+    lessons: [
+      {
+          name: 'Lesson 1',
+          slug: 'lesson-1',
+          question: new Question(
+              'Put these in an order that creates a for loop:',
+              [
+                  {
+                    order: 0,
+                    label: 'for (var i = 0; i < 10; i++)',
+                    correct: true
+                  },
+                  {
+                    order: 1,
+                    label: '{',
+                    correct: true
+                  },
+                  {
+                    order: 2,
+                    label: 'console.log(i);',
+                    correct: true
+                  },
+                  {
+                    label: 'console.log(j);',
+                    correct: false
+                  },
+                  {
+                    order: 3,
+                    label: '}',
+                    correct: true
+                  }
+                ]
+          )
+      }
+  ]
+}
+]
+
 // {
 //     name: 'Loops'
 // }
-]
+
